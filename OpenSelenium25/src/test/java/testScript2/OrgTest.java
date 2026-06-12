925cc070c73b8b4f5d392b8981ffa16e1db63550
srivi <srivi@LAPTOP-ADPTUOBU.mshome.net>
2026-06-12 12:11:58 +0530

    commit3

--- OpenSelenium25/src/test/java/testScript2/OrgTest.java
+++ OpenSelenium25/src/test/java/testScript2/OrgTest.java
@@ -8,6 +8,16 @@ public class OrgTest {
 	public void createContactTest()
 	{
 		System.out.println("execute contact test");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String User = System.getProperty("username");
+		String pass = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(User);
+		System.out.println(pass);
+		
 	}
 	@Test
 	public void modifyContactTest()
